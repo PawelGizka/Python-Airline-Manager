ea7042a0c49e23b0f0209b09413ef0df6540b13c
Pawel Gizka <pawel.gizka@scalac.io>
2018-05-10 12:25:19 +0200

    listing flight and flight details.

--- flightmanager/urls.py
+++ flightmanager/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('flights/<int:flight_id>', views.flight_details, name='flight_details'),
 ]
--- flightmanager/views.py
+++ flightmanager/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.template import loader
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
 # Create your views here.
 from django.http import HttpResponse
 
+from .models import Flight
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls 2 index.")
+    all_flights = Flight.objects.all()
+    return render(request, 'flightmanager/index.html', {'flights': all_flights})
+
+def flight_details(request, flight_id):
+    flight = get_object_or_404(Flight, pk=flight_id)
+    free_seats = flight.airplane.seat_number - flight.ticket_set.count()
+    tickets = flight.ticket_set.all()
+    return render(request, 'flightmanager/flight_details.html', {'flight': flight, 'free_seats': free_seats, 'ticekts': tickets})
+
