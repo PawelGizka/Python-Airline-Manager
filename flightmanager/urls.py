489be9758941f644b253196742aeeed142f25c64
Pawel Gizka <pawel.gizka@scalac.io>
2018-05-10 16:02:53 +0200

    add passenger to flight.

--- flightmanager/urls.py
+++ flightmanager/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name="flightmanager"
 urlpatterns = [
     path('', views.index, name='index'),
     path('flights/<int:flight_id>', views.flight_details, name='flight_details'),
+    path('flights/<int:flight_id>/add_passanger_form', views.add_passanger_form, name='add_passanger_form'),
+    path('flights/<int:flight_id>/add_passanger', views.add_passanger, name='add_passanger'),
+    path('no_tickets', views.no_tickets, name='no_tickets'),
 ]
--- flightmanager/views.py
+++ flightmanager/views.py
@@ -5,8 +5,14 @@ from django.urls import reverse
 
 # Create your views here.
 from django.http import HttpResponse
+from django.db import transaction
 
-from .models import Flight
+import logging
+
+logger = logging.getLogger(__name__)
+
+from .models import Flight, Passanger, Ticket
+# Get an instance of a logger
 
 def index(request):
     all_flights = Flight.objects.all()
@@ -17,4 +23,30 @@ def flight_details(request, flight_id):
     free_seats = flight.airplane.seat_number - flight.ticket_set.count()
     tickets = flight.ticket_set.all()
     return render(request, 'flightmanager/flight_details.html', {'flight': flight, 'free_seats': free_seats, 'ticekts': tickets})
+
+def add_passanger_form(request, flight_id):
+    flight = get_object_or_404(Flight, pk=flight_id)
+    passangers = Passanger.objects.all()
+    return render(request, 'flightmanager/add_passanger.html', {'flight': flight, 'passangers': passangers})
+
+def no_tickets(request):
+    return render(request, 'flightmanager/no_tickets.html')
+
+@transaction.atomic
+def add_passanger(request, flight_id):
+    flight = get_object_or_404(Flight, pk=flight_id)
+    free_seats = flight.airplane.seat_number - flight.ticket_set.count()
+    if free_seats == 0:
+        return HttpResponseRedirect(reverse('flightmanager:no_tickets'))
+    else:
+        passanger_id = request.POST['select']
+        passanger = get_object_or_404(Passanger, pk=passanger_id)
+        luggage_weight = request.POST['luggage_weight']
+        new_ticket = Ticket(flight_id=flight_id, passanger=passanger, luggage_weight=luggage_weight)
+        new_ticket.save()
+        return HttpResponseRedirect(reverse('flightmanager:flight_details', args=(flight_id,)))
+
+
+
+
 
